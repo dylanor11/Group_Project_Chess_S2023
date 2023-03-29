006e00862b549b5bdea55597eaaafca1cd6604a1
Dylan Barrett <dylan.barrett@slu.edu>
2023-03-28 21:08:38 -0500

    Implemented getLegalMoves() for Pawn and Knight

--- Business_Layer/Piece_Classes/Pawn.java
+++ Business_Layer/Piece_Classes/Pawn.java
@@ -19,7 +19,55 @@ public class Pawn extends Piece {
 
     @Override
     public List<Move> getLegalMoves(Board curBoard) {
-        return new ArrayList<Move>();
+        return generateLegalMoves(curBoard);
     }
 
+    private List<Move> generateLegalMoves(Board curBoard) {
+        List<Move> newMoves = new ArrayList<Move>();
+        
+        //white
+        if (color == true) {
+            if (curBoard.getBoard()[file][rank+1] == null) { // space in front is open
+                newMoves.add(new Move(true, file, rank, file, rank+1, "Pawn"));
+                if (!hasMoved && curBoard.getBoard()[file][rank+2] == null) { // should avoid any null pointer exceptions
+                    newMoves.add(new Move(true, file, rank, file, rank+2, "Pawn"));
+                }
+            }
+
+            // capturing left
+            if(file > 0 && curBoard.getBoard()[file-1][rank+1] != null && curBoard.getBoard()[file-1][rank+1].getColor() == false) {
+                newMoves.add(new Move(true, file, rank, file-1, rank+1, "Pawn"));
+            }
+
+            // capturing right
+            if(file < 8 && curBoard.getBoard()[file+1][rank+1] != null && curBoard.getBoard()[file+1][rank+1].getColor() == false) {
+                newMoves.add(new Move(true, file, rank, file+1, rank+1, "Pawn"));
+            }
+        }
+        // black
+        else {
+            if(curBoard.getBoard()[file][rank-1] == null) {
+                newMoves.add(new Move(true, file, rank, file, rank-1, "Pawn"));
+
+                if(!hasMoved && curBoard.getBoard()[file][rank-2] == null) {
+                    newMoves.add(new Move(true, file, rank, file, rank-2, "Pawn"));
+                }
+            }
+
+            // capturing left
+            if(file > 0 && curBoard.getBoard()[file-1][rank-1] != null && curBoard.getBoard()[file-1][rank-1].getColor() == true) {
+                newMoves.add(new Move(true, file, rank, file-1, rank-1, "Pawn"));
+            }
+
+            // capturing right
+            if(file < 7 && curBoard.getBoard()[file+1][rank-1] != null && curBoard.getBoard()[file+1][rank-1].getColor() == true) {
+                newMoves.add(new Move(true, file, rank, file+1, rank-1, "Pawn"));
+            }
+        }
+
+        return newMoves;
+
+        // needs to know double pawn moves to do en passant
+        // either need access to game move list or pawns need to store if they have double moved
+    }
 }
