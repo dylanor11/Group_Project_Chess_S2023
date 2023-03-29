5923b3e2fd5b17d013896fbe45f5d04a68f8bc61
Dylan Barrett <dylan.barrett@slu.edu>
2023-03-29 17:29:42 -0500

    Added freeMove() function to Game to allow moving of piece from 1 coordinate to another

--- Business_Layer/Game.java
+++ Business_Layer/Game.java
@@ -4,6 +4,7 @@ public class Game {
     private Player[] players;
     private Board board;
     private boolean curPlayer; // true if white, false if black
+    private List<Move> moves;
 
     /*
      * Creates a new game with 2 players of default names
@@ -14,6 +15,7 @@ public class Game {
         players[1] = new Player("Player 2");
         board = new Board();
         curPlayer = true;
+        moves = new ArrayList<Move>();
     }
 
     public Game(String player1, String player2) {
@@ -22,10 +24,25 @@ public class Game {
         players[1] = new Player(player2);
         board = new Board();
         curPlayer = true;
+        moves = new ArrayList<Move>();
     }
 
     public boolean getCurPlayer() { return curPlayer; }
 
     public Board getBoard() { return board; } // could also return the array of pieces with board.getBoard()
 
+    public boolean freeMove(int startFile, int startRank, int endFile, int endRank) {
+        movingPiece = board.getBoard()[startFile][startRank];
+        if(movingPiece != null) {
+            board.getBoard()[endFile][endRank] = movingPiece;
+            board.getBoard()[startFile][startRank] = null; // hopefully this doesn't destroy the moving piece
+            moves.add(new Move(curPlayer));
+            return true;
+        }
+        return false; // TODO: will return false if move is illegal
+    }
+
+    // public boolean move(int startFile, int startRank, int endFile, int endRank) {
+    //     movingPiece = board.getBoard()[startFile][startRank];
+    // }
 }
