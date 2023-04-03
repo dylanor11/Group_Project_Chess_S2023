ab9fff0ef4341de50840df7bb069229ff072de5c
MengdeZhou <mengde.zhou@slu.edu>
2023-04-03 17:07:09 -0500

    1

--- Business_Layer/Piece_Classes/King.java
+++ Business_Layer/Piece_Classes/King.java
@@ -8,6 +8,8 @@ import Group_Project_Chess_S2023.Business_Layer.Move;
 import Group_Project_Chess_S2023.Business_Layer.Piece;
 
 public class King extends Piece {
+    private boolean isChecked; // Whether the king is in check
+
     
     public King(int f, int r, boolean c) {
         file = f;
@@ -15,12 +17,43 @@ public class King extends Piece {
         color = c;
         hasMoved = false;
         name = "King";
-        symbol = 'K';
     }
 
     @Override
     public List<Move> getLegalMoves(Board curBoard) {
-        return new ArrayList<Move>();
+        List<Move> newMoves = new ArrayList<Move>();
+        int[][] moves = {{-1, -1}, {0, -1}, {-1, -1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        for (int[] move : moves) {
+
+            int newFile = file + move[0];
+            int newRank = rank + move[1];
+
+            if (isValidKingMove(curBoard,newFile, newRank)) {
+              newMoves.add(new Move(color,file, rank, newFile, newRank, "King"));
+            }
+        }
+        return newMoves;
     }
 
+
+    public boolean isValidKingMove(Board curBoard, int toFile, int toRank) {
+        Piece piece = curBoard.getBoard()[toFile][toRank];
+        if ( toFile < 0 || toFile >= 7 || toRank < 0 || toRank >= 7) {
+            return false;  // Piece out of board range, move invalid
+        }
+        if (piece.getColor() == color) {
+            return false;  // The target position is occupied by a friendly piece, move invalid
+        }
+
+
+        return true;
+    }
+
+
+
+
+
+
+
 }
+
--- Business_Layer/Piece_Classes/Rook.java
+++ Business_Layer/Piece_Classes/Rook.java
@@ -15,12 +15,89 @@ public class Rook extends Piece {
         color = c;
         hasMoved = false;
         name = "Rook";
-        symbol = 'R';
     }
 
     @Override
     public List<Move> getLegalMoves(Board curBoard) {
-        return new ArrayList<Move>();
+        List<Move> newMoves = new ArrayList<Move>();
+        int newFile;
+        int newRank;
+        int step = 1;
+        // left move
+            while (step < 8) {
+
+                newFile = file -step;
+                newRank = rank ;
+                if (isValidRookMove(curBoard,newFile, newRank)) {
+                    newMoves.add(new Move(color,file, rank, newFile, newRank, "Rook"));
+                    step++;
+                }
+                else{
+                    step = 1;
+                    break;
+                }
+
+            }
+        // right move
+        while (step < 8) {
+
+            newFile = file +step;
+            newRank = rank ;
+            if (isValidRookMove(curBoard,newFile, newRank)) {
+                newMoves.add(new Move(color,file, rank, newFile, newRank, "Rook"));
+                step++;
+            }
+            else{
+                step = 1;
+                break;
+            }
+
+        }
+        // down move
+        while (step < 8) {
+
+            newFile = file ;
+            newRank = rank - step;
+            if (isValidRookMove(curBoard,newFile, newRank)) {
+                newMoves.add(new Move(color,file, rank, newFile, newRank, "Rook"));
+                step++;
+            }
+            else{
+                step = 1;
+                break;
+            }
+
+        }
+        // up move
+        while (step < 8) {
+
+            newFile = file ;
+            newRank = rank + step;
+            if (isValidRookMove(curBoard,newFile, newRank)) {
+                newMoves.add(new Move(color,file, rank, newFile, newRank, "Rook"));
+                step++;
+            }
+            else{
+                step = 1;
+                break;
+            }
+
+        }
+          return newMoves;
     }
 
+
+
+    public boolean isValidRookMove(Board curBoard, int toFile, int toRank) {
+        Piece piece = curBoard.getBoard()[toFile][toRank];
+        if ( toFile < 0 || toFile >= 7 || toRank < 0 || toRank >= 7) {
+            return false;  // Piece out of board range, move invalid
+        }
+        if (piece.getColor() == color) {
+            return false;  // The target position is occupied by a friendly piece, move invalid
+        }
+
+
+        return true;
+    }
 }
