6115dbd72058c7204d180a1cac106bd03ceff0cb
OwenAThomasson <123423248+OwenAThomasson@users.noreply.github.com>
2023-04-12 16:44:10 -0500

    Add files via upload
    Added update LegalMoves function

--- Business_Layer/Bishop.java
+++ Business_Layer/Bishop.java
@@ -5,9 +5,107 @@ import java.lang.Math;
 public class Bishop implements Piece{
 	
 
-	public void updateLegalMoves()
+	public void updateLegalMoves(Piece[][] board, int startFile, int startRank)
 		{
-			
+			//Reset the legalmoves
+			this.legalMoves = new ArrayList<Move>;
+
+			//Direction 1
+			int checkRank = startRank + 1;
+			int checkFile = startFile + 1;
+			boolean blocked = false;
+			while(!blocked)
+			{
+				if(board[checkFile][checkRank] == null)
+				{
+					Move newMove = new Move(this.color, startFile, startRank, checkFile, checkRank);
+					legalMoves.append(newMove);
+					checkRank++ ;
+					checkFile++;
+				}
+				else
+				{
+					blocked = true;
+					if(board[checkFile][checkRank].getColor() != this.color)
+					{
+						Move newMove = new Move(this.color, startFile, startRank, checkFile, checkRank);
+						legalMoves.append(newMove);
+					}
+				}
+			}
+
+			//Direction 2
+			checkRank = startRank + 1;
+			checkFile = startFile - 1;
+			boolean blocked = false;
+			while(!blocked)
+			{
+				if(board[checkFile][checkRank] == null)
+				{
+					Move newMove = new Move(this.color, startFile, startRank, checkFile, checkRank);
+					legalMoves.append(newMove);
+					checkRank++;
+					checkFile--;
+				}
+				else
+				{
+					blocked = true;
+					if(board[checkFile][checkRank].getColor != this.color)
+					{
+						Move newMove = new Move(this.color, startFile, startRank, checkFile, checkRank);
+						legalMoves.append(newMove);
+					}
+				}
+			}
+
+			//Direction 3
+			checkRank = startRank - 1;
+			checkFile = startFile + 1;
+			boolean blocked = false;
+			while(!blocked)
+			{
+				if(board[checkFile][checkRank] == null)
+				{
+					Move newMove = new Move(this.color, startFile, startRank, checkFile, checkRank);
+					legalMoves.append(newMove);
+					checkRank--;
+					checkFile++;
+				}
+				else
+				{
+					blocked = true;
+					if(board[checkFile][checkRank].getColor != this.color)
+					{
+						Move newMove = new Move(this.color, startFile, startRank, checkFile, checkRank);
+						legalMoves.append(newMove);
+					}
+				}
+			}
+
+			//Direction 4
+			checkRank = startRank - 1;
+			checkFile = startFile - 1;
+			boolean blocked = false;
+			while(!blocked)
+			{
+				if(board[checkFile][checkRank] == null)
+				{
+					Move newMove = new Move(this.color, startFile, startRank, checkFile, checkRank);
+					legalMoves.append(newMove);
+					checkRank--;
+					checkFile--;
+				}
+				else
+				{
+					blocked = true;
+					if(board[checkFile][checkRank].getColor != this.color)
+					{
+						Move newMove = new Move(this.color, startFile, startRank, checkFile, checkRank);
+						legalMoves.append(newMove);
+					}
+				}
+			}
+
 		}
 
 	public boolean checkMove(Piece[][] board, int startFile, int startRank, int endFile, int endRank){
@@ -18,12 +116,19 @@ public class Bishop implements Piece{
 			return false;
 		}
 
+		if(board[endFile][endRank].isWhite() == this.isWhite())
+		{
+			return false;
+		}
+
 		//Next, if the absolute value of the difference of startRank and endRank does not equal absolute value of difference
 		//of startFile and endFile, it is also invalid
 
 		if(Math.abs(startFile-endFile) != Math.abs(startRank-endRank)){
 			return false;
 		}
+
+
 
 		//Finally we must figure out if it is moving left or right and if it is moving up or backwards
 
@@ -50,7 +155,7 @@ public class Bishop implements Piece{
 		}
 		int x = startRank + rankOffset;
 		int y = startFile + fileOffset;
-		for(x; x != endRow; x + rankOffset){
+		for(x; x != endRank; x + rankOffset){
 			if(board[y][x] != nullPiece){
 				return false;
 			}
